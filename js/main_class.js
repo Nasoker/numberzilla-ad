c8d609b4710fa5dc7f6e3d425496e5ca9d259496
Yaraslau Kandrachenka <nasoker@gmail.com>
2022-02-15 20:56:42 +0300

    fix: some errors add some mechanics

--- js/main_class.js
+++ js/main_class.js
@@ -19,46 +19,69 @@ class FirstScene extends Phaser.Scene {
         this.load.image('7', './assets/images/7.png');
         this.load.image('8', './assets/images/8.png');
         this.load.image('9', './assets/images/9.png');
+        this.load.image('hand','./assets/images/hand.png')
 
         /* Load Sounds */
 
         this.load.audio('main', './assets/music/main.mp3')
-        this.load.audio('error', './assets/music/Fail.mp3')
 
         /* Finish Objects */
         this.load.image('button', './assets/images/button.png');
         this.load.image('finishLogo', './assets/images/finishlogo.png');
+        this.load.image('particles', './assets/images/particles.png');
     }
     create() {
-        /* Loading Background and Logog*/
-        const bg = this.add.image(0, 0, 'bg').setOrigin(0, 0).setInteractive().setName("bg");;
+        /*  */
+
+        let isClicked = false;
+        let isChanged = false;
+        let isFinished = false;
+        let index = 0;
+
+        const createInteractiveImage = (x,y,name,interactiveName) => this.add.image(x,y,name).setOrigin(0).setInteractive().setName(interactiveName);
+        const createImage = (x,y,name) => this.add.image(x,y,name).setOrigin(0);
+
+        /* Loading Background and Logo*/
+        const bg = createInteractiveImage(0, 0, 'bg',"bg");
         const logo = this.add.image(540, 960, 'logo').setPosition(45, 0).setOrigin(0, 0).setScale(1.1);
 
         /* Text Logic*/
         const arrWithChangingStrings = ['six', 'nine', 'seven', 'four', 'two', 'eight', 'three', 'nine', 'six', 'four', 'two'];
         const arrWithChangingNumbers = [6, 9, 7, 4, 2, 8, 3, 9, 6, 4, 2];
 
-        const text = this.add.text(108, 27, `Tap ${arrWithChangingNumbers[0]    }`, { fontFamily: 'CustomFont', fontSize: '72px', fontWeight: 'bold' }).setPosition(180, 360);
+        const text = this.add.text(
+            108,
+            27,
+            `Tap ${arrWithChangingNumbers[0]}`,
+            { 
+                fontFamily: 'CustomFont', 
+                fontSize: '72px',
+                fontWeight: 'bold' 
+            }
+        ).setPosition(180, 360);
 
         /* Rectangle */
 
-        const rectangle = this.add.image(0, 529, 'main-rectangle').setOrigin(0, 0).setInteractive().setName("rectangle");
+        const rectangle = createInteractiveImage(0, 529, 'main-rectangle','rectangle');
 
         /* Load Numbers in Rectangle */
 
-        const rectangleFour = this.add.image(35, 555, '4').setOrigin(0, 0).setInteractive().setName("four");
-        const rectangleSix = this.add.image(107, 555, '6').setOrigin(0, 0).setInteractive().setName("six");
-        const rectangleThree = this.add.image(183, 555, '3').setOrigin(0, 0).setInteractive().setName("three");
-        const rectangleEight = this.add.image(256, 555, '8').setOrigin(0, 0).setInteractive().setName("eight");
-        const rectangleSeven = this.add.image(325, 555, '7').setOrigin(0, 0).setInteractive().setName("seven");
-        const rectangleNine = this.add.image(397, 555, '9').setOrigin(0, 0).setInteractive().setName("nine");
-        const rectangleTwo = this.add.image(470, 555, '2').setOrigin(0, 0).setInteractive().setName("two");
+        const four = createInteractiveImage(35, 555, '4', "four");
+        const six = createInteractiveImage(107, 555, '6', "six");
+        const three = createInteractiveImage(183, 555, '3', "three");
+        const eight = createInteractiveImage(256, 555, '8', "eight");
+        const seven = createInteractiveImage(325, 555, '7', "seven");
+        const nine = createInteractiveImage(397, 555, '9', "nine");
+        const two = createInteractiveImage(470, 555, '2', "two");
+
+        const arrWithNumbers = [six,nine,seven,four,two,eight,three,nine,six,four,two];
 
         /* End Numbers at Rectangle */
 
-        const movingRectangle = this.add.image(0, 535, 'moving-rectangle').setOrigin(0, 0).setInteractive().setName("MOVE-rectangle");
+        const movingRectangle = createInteractiveImage(0, 535, 'moving-rectangle', "MOVE-rectangle");
         const self = this;
-        this.tweens.add({
+
+        const movingRectangleAnim = this.tweens.add({
             targets: movingRectangle,
             repeat: -1,
             yoyo: true,
@@ -74,25 +97,83 @@ class FirstScene extends Phaser.Scene {
                 })
             }
         });
+
+        /* Logic Of Hint */
+
+        const showHintHand = () => {
+
+            if(isChanged){
+                index+=1;
+            }
+
+            const x = arrWithNumbers[index].x+10;
+            const y = arrWithNumbers[index].y+35;
+
+            this.hand = createImage(x,y,'hand')
+                .setScale(0.5)
+    
+            this.tweens.add({
+                targets: this.hand,
+                repeat: -1,
+                alpha: { from: 0.75, to: 1 },
+                duration: 500,
+                onStart: ()=> {
+                    isChanged = false;
+                    this.hand.setVisible(true);
+                },
+                onComplete:()=>{
+                    self.tweens.add({
+                        targets: this.hand,
+                        repeat: 0,
+                        duration: 500,
+                        alpha: { from: 1, to: 0.75 },
+                    })
+                }
+            });
+        }
+
+        if (!isClicked){
+            this.firstHint = this.time.addEvent({ 
+                delay: 2000, 
+                callback: showHintHand,
+                loop: false 
+            });
+        }
+
+        /* Finish Logic Of Hint */
+
         this.input
             .setTopOnly(false)
             .on(
                 'pointerdown',
-                (
-                    pointer,
+                (   pointer,
                     objectsClicked
                 ) => {
                     objectsClicked.forEach((obj) => {
-                        if (obj.name === arrWithChangingStrings[0]) {
+                        if (obj.name === arrWithChangingStrings[0] && objectsClicked.length === 3) {
+                            isChanged = true;
+
+                            if(this.hand){
+                                this.hand.setVisible(false);
+                            }
+
+                            this.firstHint.remove();
+
+                            if(!isFinished){
+                                this.hint = this.time.addEvent({ 
+                                    delay: 2000, 
+                                    callback: showHintHand,
+                                    loop: false 
+                                });
+                            }else{
+                                this.hint.remove();
+                            }
+
                             arrWithChangingNumbers.splice(0, 1);
                             arrWithChangingStrings.splice(0, 1);
+
                             text.setText(`Tap ${arrWithChangingNumbers[0]}`)
                         }
-                        /* if (Object.keys(objectsClicked).length < 4) {
-                            errorSound.play();
-                        } else if (obj.name !== arrWithChangingStrings[0]) {
-                            errorSound.play();
-                        } */
                     })
                 }
             );
@@ -100,21 +181,37 @@ class FirstScene extends Phaser.Scene {
         /* Load and play Sound  */
 
         const mainSound = this.sound.add('main');
-        /* const errorSound = this.sound.add('error'); */
         /* mainSound.play(); */
 
         /* LOAD LAST SCENE */
-        const darkenRectangle = this.add.rectangle(0, 0, 540, 960, 0x404040).setAlpha(0).setDisplaySize(540, 960).setOrigin(0).setVisible(0);
-        const finishLogo = this.add.image(0, 0, 'finishLogo').setOrigin(0).setVisible(0).setDisplaySize(540, 960);
-        const button = this.add.image(100, 680, 'button').setOrigin(0, 0).setVisible(0);
+        const darkenRectangle = this.add.rectangle(0, 0, 540, 960, 0x404040)
+            .setAlpha(false)
+            .setDisplaySize(540, 960)
+            .setOrigin(0)
+            .setVisible(0);
+
+        const finishLogo = this.add.image(0, 0, 'finishLogo')
+            .setOrigin(0)
+            .setVisible(false)
+        
+        const particles = this.add.image(0,0,'particles')
+            .setOrigin(0, 0)
+            .setVisible(false);
+
+        const button = this.add.image(100, 680, 'button')
+            .setOrigin(0, 0)
+            .setVisible(false);
+
         const visibleTime = () => {
-            console.log('started');
             self.tweens.add({
                 targets: darkenRectangle,
-                alpha: { from: 0, to: 0.95 },
+                alpha: { from: 0, to: 0.97 },
                 duration: 1250,
                 onStart: () => {
-                    darkenRectangle.setVisible(1);
+                    isFinished = true;
+                    self.hand.setVisible(false);
+                    darkenRectangle.setVisible(true);
+                    movingRectangleAnim.pause();
                     self.tweens.add({
                         targets: logo,
                         alpha: { from: 1, to: 0 },
@@ -123,18 +220,31 @@ class FirstScene extends Phaser.Scene {
                 },
                 onComplete: () => {
                     self.tweens.add({
-                        targets: [finishLogo, button],
+                        targets: [finishLogo, button,particles],
                         alpha: { from: 0, to: 1 },
                         duration: 750,
                         onStart: () => {
-                            finishLogo.setVisible(1);
-                            button.setVisible(1);
+                            finishLogo.setVisible(true);
+                            button.setVisible(true);
+                            particles.setVisible(true);
                         },
+                        onComplete: ()=> {
+                            particlesAnim()
+                        }
                     });
                 }
             });
         }
-        this.time.addEvent({ delay: 10000, callback: visibleTime, loop: false });
+        const particlesAnim = () => {
+            this.tweens.add({
+                targets: particles,
+                repeat: -1,
+                alpha: { from: 1, to: 0.5 },
+                duration: 500,
+                yoyo:true,
+            });
+        }
+        this.finishEvent = this.time.addEvent({ delay: 3000, callback: visibleTime, loop: false });
     }
 }
 
